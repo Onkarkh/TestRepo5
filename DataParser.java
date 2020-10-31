a5e63b890fd16ec5fd9e49f277282dd16a5e789f
Onkar <khaladkaronkar@gmail.com>
2020-10-31 08:08:55 +0530

    Unique Ip, date, code

--- DataParser.java
+++ DataParser.java
@@ -4,23 +4,21 @@ class DataParser{
 	
 	//109.169.248.247 - - [12/Dec/2015:18:25:11 +0100] "GET /administrator/ HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"	
 		
-		static String[] retrivedData = new String[20];
-		static String[] ipAddress = new String[20];
-		static String[] dates = new String[20];
-		static String[] code = new String[20];
+		static List<String> retrivedData = new ArrayList<String>(); //Used Arraylist instead of Array[] due to Indexing issue.
+		static HashSet<String> uniqueIpAddress = new HashSet<String>(); // Used HashSet to Print only Unique Data.
+		static HashSet<String> uniqueDates = new HashSet<String>();
+		static HashSet<String> serverCode = new HashSet<String>();
 	
 	
 	public static void readData(){
 	
 		BufferedReader reader;
 		try {
-			reader = new BufferedReader(new FileReader("logs_sm.txt"));
+			reader = new BufferedReader(new FileReader("logs_sm.csv"));
 			String line = reader.readLine().trim();
-			int i = 0;
 			while (line != null) {
-				retrivedData[i] = line;
+				retrivedData.add(line);
 				line = reader.readLine();
-				i++;
 			}
 			reader.close();
 		} catch (IOException e) {
@@ -28,41 +26,51 @@ class DataParser{
 		}
 	}
 	
-	public static void getIpAddress(){
-		for(int i = 0; i<=retrivedData.length; i++){
-			ipAddress[i] = retrivedData[i].split(" - - ")[0];
-			System.out.println(ipAddress[i]);
-		}	
-	}
-	
-	public static void getDates(){
-		String[] temp = new String[20];
-		for(int i = 0; i<retrivedData.length; i++){
-			temp[i] = retrivedData[i].split(" +")[3];
-			temp[i] = temp[i].split(":")[0];
-			dates[i] = temp[i].split("\\[")[1];
-			System.out.println(dates[i]);
+	public static void getUniqueIpAddresses(){
+		for(String ip : retrivedData){
+			String temp = ip.split(" - - ")[0];
+			uniqueIpAddress.add(temp);
 		}
 	}
+		
+	public static void getDates(){
+		String temp = "";
+		for(String dates : retrivedData){
+			temp = dates.split(" +")[3];
+			temp = temp.split(":")[0];
+			temp = temp.split("\\[")[1];
+			uniqueDates.add(temp);
+		}
+	}
+	
 	
 	public static void getCode(){
-		String[] temp = new String[20];
-		for(int j = 0; j<retrivedData.length; j++){
-			code[j] = retrivedData[j].split(" +")[8];
-			System.out.println(code[j]);
+		String temp = "";
+		for(String code : retrivedData){
+			temp = code.split(" +")[8];
+			if(temp!="200"){
+				serverCode.add(temp);
+			}
 		}
 	}
 	
+	public static void printData(HashSet<String> printMap){
+		for(String i : printMap){
+            System.out.println(i);
+        }
+	}
 
 public static void main(String args[]){
 	
 		readData();
 		
-		getIpAddress();
+		getUniqueIpAddresses();
 		
-		//getDates();
+		getDates();
 		
-		//getCode();
+		getCode();
+		
+		printData(uniqueIpAddress); // Pass the HashSet uniqueIpAddress, uniqueDates, serverCode to Print
 		
 	}
 	
